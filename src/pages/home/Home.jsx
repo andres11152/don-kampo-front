ceffe6c329438d9f61c0e983653b4635d166b043
Andres Betancourt <andres11152@gmail.com>
2024-12-18 22:19:30 -0500

    change api localhost to webservice render

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
   const fetchProducts = async (query) => {
     try {
       const response = await axios.get(
-        `http://localhost:8080/api/products?search=${query}`,
+        `https://don-kampo-api.onrender.com/api/products?search=${query}`,
         { withCredentials: true }
       );
   
