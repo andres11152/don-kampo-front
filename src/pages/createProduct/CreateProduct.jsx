f898d731736d711b2529abb074401e465eac4314
Andres Betancourt <andres11152@gmail.com>
2025-01-05 09:07:19 -0500

    new changes

--- src/pages/createProduct/CreateProduct.jsx
+++ src/pages/createProduct/CreateProduct.jsx
@@ -21,181 +21,6 @@
 
   const { Option } = Select;
 
-<<<<<<< Updated upstream
-const CreateProduct = () => {
-  const [form] = Form.useForm();
-  const [imageFile, setImageFile] = useState(null);
-  const [isLoading, setIsLoading] = useState(false)
-  const [processExcel, setProcessExcel] = useState({message: '', status: ''})
-  const [messageButton, setMessageButton] = useState('Continuar')
-  const [updateProducts, setUpdateProducts] = useState([])
-  const [variations, setVariations] = useState([
-    {
-      quality: "",
-      quantity: "",
-      price_home: "",
-      price_supermarket: "",
-      price_restaurant: "",
-      price_fruver: "",
-    },
-  ]);
-  const [values, setValues] = useState({'name': '', 'category': '', 'description': ''})
-
-
-  const processingExcel = async (file) => {
-    const sleep = ms => new Promise(r => setTimeout(r, ms));
-    setIsLoading(true)
-    setProcessExcel({message: 'Procesando archivo Excel', status: ''})
-    await sleep(2000)
-    const reader = new FileReader();
-  
-    reader.onload = async (e) => {
-      try {
-        const data = new Uint8Array(e.target.result);
-        const workbook = XLSX.read(data, { type: 'array' });
-  
-        // Leer la hoja Products
-        const productSheet = workbook.Sheets['Products'];
-        if (!productSheet) {
-          setProcessExcel({message: 'La hoja Products no existe.', status: 'error'})
-          throw new Error('La hoja Products no existe en el archivo Excel.');
-        }
-  
-        const productData = XLSX.utils.sheet_to_json(productSheet, { header: 1 });
-        if (productData.length < 2) {
-          setProcessExcel({message: 'La hoja Products no contiene datos.', status: 'error'})
-          throw new Error('La hoja Products no contiene datos suficientes.');
-        }
-        
-        const headers = productData[0];
-        const productRows = productData.slice(1).filter(row => row.length > 0);
-        
-        // Convertir filas a objetos
-        const excelProducts = productRows.map((row) => {
-          const product = {};
-          headers.forEach((header, index) => {
-            product[header] = row[index];
-          });
-          return {
-            product_id: product['Id'],
-            name: product['Nombre'],
-            description: product['Descripcion'],
-            category: product['Categoria'],
-            photo_url: null,
-            stock: product['Stock'],
-            variations: [], // Inicialmente vacío, se llenará más adelante
-          };
-        });
-  
-        setProcessExcel({message: 'Productos obtenidos de excel.', status: ''})
-        await sleep(2000)
-
-        // obtengo todos los productos
-        const response = await axios.get("http://localhost:8080/api/products", { withCredentials: true });
-        const products = response.data
-        // Filtro los productos cuyo id sea igual a los de la base de datos
-        const totalExcelProducts = excelProducts.filter(excelProduct => products.some(product => product.product_id === excelProduct.product_id));
-                
-        // Leer hojas de Variation
-        const variationSheets = Object.keys(workbook.Sheets).filter((sheetName) =>
-          /^Variation \d+$/.test(sheetName)
-        );
-  
-        variationSheets.forEach((sheetName) => {
-          const variationSheet = workbook.Sheets[sheetName];
-          const variationData = XLSX.utils.sheet_to_json(variationSheet, { header: 1 });
-  
-          if (variationData.length < 2) {
-            console.warn(`La hoja ${sheetName} no contiene datos suficientes.`);
-            setProcessExcel({message: 'La hoja ${sheetName} no contiene datos suficientes.', status: 'warning'})
-            return;
-          }
-  
-          const variationHeaders = variationData[0];
-          const variationRows = variationData.slice(1);
-  
-          variationRows.forEach((row) => {
-            const variation = {};
-            variationHeaders.forEach((header, index) => {
-              variation[header] = row[index];
-            });
-  
-            const product = totalExcelProducts.find((p) => p.product_id === variation['Id Product']);
-            if (product) {
-              product.variations.push({
-                variation_id: variation['Id Variation'],
-                quality: variation['Calidad'],
-                quantity: variation['Cantidad'],
-                price_home: variation['Hogar'],
-                price_supermarket: variation['Supermercado'],
-                price_restaurant: variation['Restaurant'],
-                price_fruver: variation['Fruver'],
-              });
-            }
-          });
-        });
-
-        setProcessExcel({message: 'Variaciones obtenidas de excel.', status: ''})
-        await sleep(1000)
-        setProcessExcel({message: `${totalExcelProducts.length} de ${excelProducts.length} productos por actualizar `, status:'info'})
-        setMessageButton(`Actualizar Productos`)
-        setUpdateProducts(totalExcelProducts)
-      } catch (error) { 
-        console.error('Error procesando el archivo Excel:', error); 
-        setProcessExcel({message: 'Error procesando el archivo Excel' + error, status: 'error'})
-        setMessageButton('Cancelar')
-      }
-    };
-  
-    reader.readAsArrayBuffer(file);
-  };
-  
-  const uploadProps = {
-    accept: ".xlsx, .xls",
-    beforeUpload: (file) => {
-      processingExcel(file);
-      return false;
-    }
-  };
-
-  const updatingProducts = (event) => {   
-    event.preventDefault()
-    console.log(updateProducts);
-    
-    setProcessExcel({message: 'Actualizando productos', status: ''})
-    axios.put("http://localhost:8080/api/updatemultipleproducts", updateProducts, {
-      headers: { 'Content-Type': 'application/json' },
-    })
-    .then((response) => {
-      setProcessExcel({message: 'Productos actualizados exitosamente', status: 'success'})
-      setMessageButton('Continuar')
-    })
-    .catch((error) => {
-      console.error("Error procesando el archivo Excel:", error);
-      setProcessExcel({message: 'Error procesando el archivo Excel', status: 'error'})
-      setMessageButton('Cancelar')
-    });
-  }
-
-  const closeModal = () => {
-    setIsLoading(false);
-    processExcel.status === 'success' && window.location.reload();
-  }
-
-  const handleImageUpload = ({ file }) => {
-    file && setImageFile(file)    
-  };
-
-  const handleVariationChange = (index, field, value) => {
-    const updatedVariations = [...variations];
-    updatedVariations[index] = { ...updatedVariations[index], [field]: value };
-    setVariations(updatedVariations);
-  };
-
-  const addVariation = () => {
-    setVariations([
-      ...variations,
-=======
   const CreateProduct = () => {
     const [form] = Form.useForm();
     const [imageFile, setImageFile] = useState(null);
@@ -204,7 +29,6 @@ const CreateProduct = () => {
     const [messageButton, setMessageButton] = useState('Continuar')
     const [updateProducts, setUpdateProducts] = useState([])
     const [variations, setVariations] = useState([
->>>>>>> Stashed changes
       {
         quality: "",
         quantity: "",
@@ -295,23 +119,6 @@ const CreateProduct = () => {
                 variation[header] = row[index];
               });
     
-<<<<<<< Updated upstream
-    // Adjuntar la imagen como binary
-    imageFile && formData.append("photo_url", imageFile);
-  
-    // Convertir el objeto `productData` a JSON y adjuntarlo al FormData
-    Object.keys(productData).forEach(key => {
-      key === "variations" ? formData.append(key, JSON.stringify(productData[key]))
-      : formData.append(key, productData[key]);
-    });    
-  
-    try {
-      const response = await axios.post(
-        "http://localhost:8080/api/createproduct",
-        formData,
-        {
-          headers: { "Content-Type": "multipart/form-data" },
-=======
               const product = totalExcelProducts.find((p) => p.product_id === variation['Id Product']);
               if (product) {
                 product.variations.push({
@@ -336,7 +143,6 @@ const CreateProduct = () => {
           console.error('Error procesando el archivo Excel:', error); 
           setProcessExcel({message: 'Error procesando el archivo Excel' + error, status: 'error'})
           setMessageButton('Cancelar')
->>>>>>> Stashed changes
         }
       };
     
@@ -540,19 +346,7 @@ const CreateProduct = () => {
                 },
               ]}
             >
-<<<<<<< Updated upstream
-              <Select onChange={(value) => handleValues('category', value)} placeholder="Selecciona una categoría">
-                <Option value="Frutas importadas">Frutas importadas</Option>
-                <Option value="Verdura">Verdura</Option>
-                <Option value="Frutas nacionales">Frutas nacionales</Option>
-                <Option value="Cosecha">Cosecha</Option>
-                <Option value="Hortalizas">Hortalizas</Option>
-                <Option value="Promociones">Promociones</Option>
-                <Option value="Otros">Otros</Option>
-              </Select>
-=======
               <Input.TextArea onChange={(e) => handleValues('description', e.target.value)} placeholder="Descripción del producto" />
->>>>>>> Stashed changes
             </Form.Item>
             
             <Form.Item label="Foto del Producto">
